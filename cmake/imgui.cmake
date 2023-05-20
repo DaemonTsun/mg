e2dc763aac54bfe36ad296a3b0bbdaa01adc2aa1
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-05-20 16:26:23 +0200

    Move imgui_demo dependency to demo only

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -4,7 +4,7 @@ set(imgui_SOURCES
     "${imgui_SOURCES_DIR}/imgui.h"
     "${imgui_SOURCES_DIR}/imgui.cpp"
 
-    "${imgui_SOURCES_DIR}/imgui_demo.cpp"
+#   "${imgui_SOURCES_DIR}/imgui_demo.cpp"
     "${imgui_SOURCES_DIR}/imgui_draw.cpp"
     "${imgui_SOURCES_DIR}/imgui_widgets.cpp"
     "${imgui_SOURCES_DIR}/imgui_tables.cpp"
@@ -18,13 +18,4 @@ elseif (WINDOW STREQUAL "GLFW")
     set(imgui_SOURCES ${imgui_SOURCES} "${imgui_SOURCES_DIR}/backends/imgui_impl_glfw.cpp")
 endif()
     
-# add_library(imgui STATIC)
-# set_property(TARGET imgui PROPERTY CXX_STANDARD 11)
-# target_sources(imgui PRIVATE ${imgui_SOURCES})
-# target_include_directories(imgui PRIVATE "${imgui_SOURCES_DIR}"
-#                                         ${WINDOW_INCLUDE_DIRECTORIES})
-
-# target_link_libraries(imgui ${WINDOW_LIBRARIES} ${Vulkan_LIBRARIES})
-
-# set(imgui_LIBRARIES imgui)
 set(imgui_INCLUDE_DIRECTORIES "${imgui_SOURCES_DIR}")
